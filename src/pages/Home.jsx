39256ed863ac9c8ad1dbee0ea0e4a111bdfb41ed
unknown <Manuella Bell>
2026-07-12 04:37:02 +0100

    Version finale avant déploiement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Bienvenue sur BellNova 🚀</h1>
-      <p>Nous développons des sites web modernes, des applications mobiles et des solutions digitales pour les particuliers et entreprises.</p>
+      <h1>Nous développons des sites web modernes, des applications mobiles et des solutionq digitalzq pour les particuliers et entrerpises. 🚀</h1>
     </div>
   );
 }
